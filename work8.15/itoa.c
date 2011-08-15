6d8273e443ebf9df0f588364ca3a56f198bbbbb9
althars <xkjh2003@yahoo.com.cn>
2011-08-15 16:11:51 +0800

    itoa 程序进行小改动
    改正不能使用0的bug

--- work8.15/itoa.c
+++ work8.15/itoa.c
@@ -8,11 +8,11 @@ char * itoa(int i,char * ch)
     int sign;
     int time=0;
     int tmp=i;
-    while(i!=0)
+    do
     {
        i=i/10;
        time=time+1;
-    }
+    } while(i!=0);
     sign=time-1;
     while(sign!=-1)
     {
